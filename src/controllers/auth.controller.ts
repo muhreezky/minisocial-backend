1eaf1eab768b1547e15399cab27f236bf70cb1c8
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-08-30 14:21:25 +0700

    fix(api): dapat login dengan email atau username

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,22 +1,34 @@
 import { Request, Response } from 'express';
 import { login, newAccount } from '../services/auth.service';
 
-export async function registerUser (req: Request, res: Response) {
+export async function registerUser(req: Request, res: Response) {
   try {
     const { email, username, password } = req.body;
     const user = await newAccount(email, username, password);
-    return res.status(201).json({ message: 'Akun berhasil dibuat', data: { user } });
+    if (!user)
+      return res.status(400).json({
+        status: 400,
+        message: 'Username atau E-mail sudah ada',
+        data: null,
+      });
+    return res
+      .status(201)
+      .json({ message: 'Akun berhasil dibuat', data: { user } });
   } catch (e: unknown) {
-    return res.status(500).json({ message: 'Kesalahan Internal pada Halaman', error: e });
+    return res
+      .status(500)
+      .json({ message: 'Kesalahan Internal pada Halaman', error: e });
   }
 }
 
-export async function loginUser (req: Request, res: Response) {
+export async function loginUser(req: Request, res: Response) {
   try {
     const { email, password } = req.body;
     const user = await login(email, password);
     return res.status(200).json({ message: 'Login berhasil', data: { user } });
   } catch (e: unknown) {
-    return res.status(500).json({ message: 'Kesalahan Internal pada Halaman', error: e });
+    return res
+      .status(500)
+      .json({ message: 'Kesalahan Internal pada Halaman', error: e });
   }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,22 +1,30 @@
 import * as bcrypt from 'bcryptjs';
 import prisma from '../utils/prisma';
 
+// Mengambil data user
+export async function getUserData(cred: string, uname?: string) {
+  const user = await prisma.user.findFirst({ 
+    where: {
+      OR: [{ email: cred }, { username: uname || cred }],
+    },
+  });
+  return user;
+}
+
 export async function newAccount(email: string, username: string, password: string) {
   try {
+    const exists = !!(await getUserData(email, username));
+    if (exists) return null;
     const salt = await bcrypt.genSalt(6);
     const hashed = await bcrypt.hash(password, salt);
     const account = await prisma.user.create({
       data: { email, password: hashed, username },
     });
     return account;
-  } catch (e: unknown) {
+  } catch (e: any) {
+    console.log(e?.stack);
     throw e;
   }
-}
-
-export async function getUserData(email: string) {
-  const user = await prisma.user.findFirst({ where: { email } });
-  return user;
 }
 
 export async function login(email: string, password: string) {
