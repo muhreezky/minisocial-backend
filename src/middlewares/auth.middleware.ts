3b28426e7db4dbf28f398807d1b1af0def1f09ca
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-08-31 10:52:11 +0700

    fix: ketinggalan

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response, response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+function unauthed () {
+	return response.status(401).json({ message: 'Maaf, anda tidak boleh masuk' });
+}
+
+export type CustomRequest = Request & {
+	token: string | JwtPayload;
+}
+
+export function verifyUser (req: Request, res: Response, next: NextFunction) {
+	try {
+		const bearer = req.headers.authorization;
+		if (!bearer) return unauthed();
+		const token = bearer.split(' ')[1];
+		if (!token) return unauthed();
+		const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET as string);
+		(req as CustomRequest).token = decoded;
+		next();
+	} catch (e: any) {
+		return res.status(500).json({ message: e.message, error: e });
+	}
+}
