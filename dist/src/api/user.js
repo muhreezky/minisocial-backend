02b3fe4b475189e8288267c80256943591c854a5
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-09-10 10:08:07 +0700

    fix: ubah lagi agi

--- dist/src/api/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_controller_1 = require("../controllers/user.controller");
-const express_1 = require("express");
-const userRoute = (0, express_1.Router)();
-userRoute.get('/:username', user_controller_1.getUserByUsername);
-exports.default = userRoute;
-//# sourceMappingURL=user.js.map
