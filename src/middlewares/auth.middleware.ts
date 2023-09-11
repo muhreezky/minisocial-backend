d21f2acf870609507dc6baab7db536def7af7c51
muhreezky <82578987+muhreezky@users.noreply.github.com>
2023-09-11 08:40:45 +0700

    fix: coba lawan error

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,16 +1,13 @@
-import { response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
-function unauthed () {
-	return response.status(401).json({ message: 'Maaf, anda tidak boleh masuk' });
-}
+const message = 'Maaf anda tidak diizinkan untuk masuk';
 
 export function verifyUser (req: any, res: any, next: any) {
 	try {
 		const bearer = req.headers.authorization;
-		if (!bearer) return unauthed();
+		if (!bearer) return res.status(401).json({ message });
 		const token = bearer.split(' ')[1];
-		if (!token) return unauthed();
+		if (!token) return res.status(401).json({ message });
 		const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET as string);
 		req.token = decoded as JwtPayload;
 		next();
